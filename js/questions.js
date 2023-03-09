ad0f1d80330c6a5c2925e78adac4d6f61d3b3a5c
Beverley Witherigton <beverleywitherington@gmail.com>
2023-03-09 13:19:04 +1100

    Finished Quiz

---
+++ js/questions.js
@@ -0,0 +1,64 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+     question: "How to write an IF statement in JavaScript?",
+      answer: "if (i == 5) ",
+        options: [
+        "if 1 = 5",
+        "if 1 = 5 then",
+        "if (i == 5) ",
+        "if 1 == 5 then"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the correct way to call a string's built-in method?",
+    answer: "'javascript'.toUpperCase();",
+    options: [
+      "'javascript'.toUpperCase();",
+      "toUpperCase.('javascript');",
+      "toUpperCase'.'javascript';",
+      "'javascript'.toUpperCase;"
+    ]
+  },
+     {
+    numb: 3,
+    question: "What is the outcome of this statement;console.log(hi!.length)?",
+    answer: "3 is printed to the console",
+    options: [
+      "1 is printed to the console",
+      "3 is printed to the console",
+      "'hi!'.length is printed to the console",
+      "'hi!'.length will be evaluated to 3 and will print to the console"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is interpolation?",
+    answer: "Using template literals to embed variables into strings",
+    options: [
+      "Printing a string to the console",
+      "Using template literals to embed variables into strings",
+      "Changing the value of a variable",
+      "Joining multiple strings togther using operators like +"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the correct way to call the random method on the Math Global Object?",
+    answer: "Math.random()",
+    options: [
+      "math.random()",
+      "random.Math()",
+      "Math.random()",
+      "Math(random)"
+    ]
+  }
+];
+     
+    
+    
+
+    
+   
